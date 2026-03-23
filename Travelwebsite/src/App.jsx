17df96c2a6af77dd093b7ada5f55d3ebbe94ac1a
ParthSingh112 <technoxian543@gmail.com>
2026-03-23 10:58:50 +0530

    first commit

---
+++ Travelwebsite/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App(){
+  return(
+    <>
+      <Navbar/>
+      <Header/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
